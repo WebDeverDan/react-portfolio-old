d266400c11554d42c4fdb943bee2bb8010f29cef
Daniel Bradley <danielbvideo@gmail.com>
2021-10-19 23:21:58 -0400

    updated header

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const Header = ({title}) => {
-    // const onClick = () => {
-    //     console.log('click')
-    // }
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <div id="navigation">
-                <ul>
-                    <li><a href="#about-me">About Me</a></li>
-                    <li><a href="#work">Work</a></li>
-                    <li><a href="#contact-me">Contact</a></li>
-                    <li>
-                    <a
-                        href="https://docs.google.com/document/d/14yti_k0GQCLeKrqTCVWKKsfx3TdvEYrY/edit"
-                        >Resume</a>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    )
-}
+const Header = ({ title }) => {
+  // const onClick = () => {
+  //     console.log('click')
+  // }
+  return (
+    <header className="header">
+      <div className="page_title">
+      <h1>{title}</h1>
+      </div>
+      <div class="container">
+  <nav class="navbar navbar-collapse navbar-light">
+      <div className="container-fluid d-flex justify-content-end">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Navbar</a>
+      </div>
+  </nav>
+</div>
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Daniel Bradley',
-}
+  title: 'Daniel Bradley',
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
