468c7b2d3b28bd7e07f4657618a91450a695bb79
Daniel Bradley <danielbvideo@gmail.com>
2021-10-22 11:25:35 -0400

    added routing and header component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,15 +1,56 @@
-// import Header from './components/Header';
-import Title from './components/TitlePage';
+import Home from './components/Home';
+import About from './components/About';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {<Title />}
-      {/* <Header /> */}
-    </div>
+    <Router>
+      <div className="App">
+      
+        
+        { window.location.pathname !==  "/" ? <Header />:""}
+        
+        {/* <About /> 
+          <Portfolio />  */}
+
+        {/* <Link to="/portfolio">
+          Portfolio
+        </Link> */}
+        {/* <Link to="/resume">
+          Resume
+        </Link>
+        <Link to="/contact">
+          Contact
+        </Link>  */}
+      
+
+      
+        {/* <Route path="/" exact>
+          <Home />
+        </Route> */}
+        <Route exact path="/">
+          <Home />
+        </Route> 
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route> 
+        {/* <Route path="/resume" exact>
+          <About />
+        </Route>
+        <Route path="/contact" exact>
+          <About />
+        </Route>  */}
+      </div>
+    </Router>
   );
 }
 
