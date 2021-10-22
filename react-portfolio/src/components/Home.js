57362dc5341d1ecbc995cfba0edd4e283e9f4307
Daniel Bradley <danielbvideo@gmail.com>
2021-10-22 11:27:35 -0400

    added home component

---
+++ react-portfolio/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Home = () => {
+  
+  return (
+    <div className="home-container home-text">
+    <div className="title-header">
+      <h1>Daniel Bradley</h1>
+    </div>
+    <div className="title-page">
+        <h1 className="web-developer">Web Developer</h1>
+        <a className="navbar-brand enter-btn" href="/about">Enter</a>
+    </div>
+    </div>
+  );
+};
+
+
+export default Home;
