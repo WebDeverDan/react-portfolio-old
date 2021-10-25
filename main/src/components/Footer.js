d83e5f73037953f86d7eca8236dc26effd79978e
Daniel Bradley <danielbvideo@gmail.com>
2021-10-25 15:20:12 -0400

    updated footer for responsiveness

--- main/src/components/Footer.js
+++ main/src/components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div class='foot-wrapper'>
-      <footer class='footer banner navbar-dark bg-dark text-light bg-opacity-50 p-2'>
-        <p className='footer_text'>Copyright: 2021 - Daniel Bradley</p>
+    <div class='foot-wrapper '>
+      <footer class='footer banner navbar-dark bg-dark text-light p-2'>
+        <p className='footer_text'>Copyright: 2021 - Daniel Bradley <i class="devicon-github-original"></i> <i class="devicon-linkedin-plain"></i> </p>
+        
+
+
       </footer>
     </div>
     
