1d0716335dd6ea64639727cb5b12335d3a16945a
Daniel Bradley <danielbvideo@gmail.com>
2021-10-25 15:20:44 -0400

    updated home for alignment

--- main/src/components/Home.js
+++ main/src/components/Home.js
@@ -4,11 +4,11 @@ import React from 'react';
 const Home = () => {
   
   return (
-    <div className="home-container home-text">
-    <div className="title-header">
-      <h1>Daniel Bradley</h1>
+    <div className="home-container home-text text-center">
+    <div className="title-header text-center">
+      <h1 className="mt-5">Daniel Bradley</h1>
     </div>
-    <div className="title-page">
+    <div className="title-page mt-10">
         <h1 className="web-developer">Web Developer</h1>
         <a className="navbar-brand enter-btn" href="/about">Enter</a>
     </div>
