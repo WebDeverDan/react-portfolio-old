cd001634e9410ab3470e2f088ea7096041c0efef
Daniel Bradley <danielbvideo@gmail.com>
2021-10-25 15:21:19 -0400

    updated resume file and changed styling

--- main/src/components/Resume.js
+++ main/src/components/Resume.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import Resume2 from '../Images/Daniel_Bradley_Resume.pdf';
-
+import Resume2 from '../Images/Daniel Bradley Resume (1).pdf';
+import { Container } from 'react-bootstrap';
 
 const Resume = () => {
   return (
-    <div class="container bg-dark bg-opacity-50 portfolio-container mt-5 mb-5">
-      <h1 class="display-4 text-light">Resume</h1>
-      <iframe src={Resume2} width="100%" height="500px">
+    // <div class="container bg-dark bg-opacity-50 portfolio-container mt-5 mb-5">
+    <Container fluid="md bg-dark bg-opacity-75 mt-5 mb-5 resume-container">
+      <h1 class="display-4 text-light text-center">Resume</h1>
+      <iframe src={Resume2} width="100%" height="500px ">
     </iframe>
       
-      </div>
+    </Container >
+      // </div>
     
   );
 };
