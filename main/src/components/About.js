9039340634f4fc0198d3d77133da52b85e8135ee
Daniel Bradley <danielbvideo@gmail.com>
2021-10-26 13:41:14 -0400

    added spacing and second paragraph

--- main/src/components/About.js
+++ main/src/components/About.js
@@ -6,26 +6,22 @@ const About = () => {
  
   return (
    
-<Container fluid="md bg-dark bg-opacity-75 mt-5 mb-5">
+<Container fluid="md bg-dark bg-opacity-75 mt-5 mb-5 about-info">
 {/* <div class="container bg-dark bg-opacity-50 portfolio-container mt-5 mb-5"> */}
-    <h1 class="display-4 text-light">About Daniel</h1>
-    <Row className="justify-content-center">
+    <h1 class="display-5 text-light text-center">About Daniel</h1>
+    <Row className="justify-content-center about-details">
     {/* <div className="about-content container-fluid bg-opacity-50"> */}
     <Col sm={8}>
-    <p class="lead display-6 text-light">I am a full stack web developer with a front-end focus, and carry a background in visual media production, business, and client relations. I bring my strong design background, knack for user experience, and understanding of full stack web application development to the table.</p>
+    <p class="lead text-light display-10" style={{fontSize: 25}}>I am a full stack web developer with a front-end focus, and carry a background in visual media production, business, and client relations. While currently seeking a career change, I am bringing my strong design background, knack for user experience, and understanding of full stack web application development to the table.</p>
+    <p class="lead text-light mt-2" style={{fontSize: 25}}>When I'm not coding, I enjoy photography, running, and rollerblading with my dog, Annie. I look forward to exploring future projects with you!</p>
 
     </Col>
-    <Col sm={4}>
-    <img src={Daniel} alt="image of Daniel who is wearing a green shirt" className="Daniel mb-3" />
+    <Col sm={3}>
+    <img src={Daniel} alt="image of Daniel who is wearing a green shirt" className="Daniel mb-5"/>
     </Col>
     </Row>
     {/* </div> */}
-    <div className="skills  text-light bg-opacity-25">
-    <h2 class="lead display-4 text-light">Front End</h2>
-    <p class="lead text-light display-6">HTML5, CSS, JQuery, JavaScript, Bootstrap, Materialize, Bulma, React, Handlebars</p>
-    <h2 class="lead display-4 text-light">Back End</h2>
-    <p class="lead text-light display-6">Node.js, Express.js, MySQL, Sequelize, MongoDB, GraphQL, authentication and varification.</p>
-</div>
+    
 </Container>
   );
 };
