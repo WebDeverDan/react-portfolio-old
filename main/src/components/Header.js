fa7b7ef42e93e7bf77335ec60bf145c86cfeed6d
Daniel Bradley <danielbvideo@gmail.com>
2021-10-26 13:42:11 -0400

    updated header font

--- main/src/components/Header.js
+++ main/src/components/Header.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+
+
+
 
 const Header = () => {
+  // const location = useLocation();
+
+   
   return (
     
-    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark d-flex justify-content-between">
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark d-flex justify-content-between">
   <div className="nav-title">
-  <a class="text-light title-header">Daniel Bradley</a>
+  <h1 class="text-light title-header" style={{fontSize: 35}}>Daniel Bradley - Web Developer</h1>
   </div>
   <div className="nav-items">
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse  p-4" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active text-light header-text" href="/about">About</a>
-      <a class="nav-item nav-link text-light header-text" href="/portfolio">Portfolio</a>
-      <a class="nav-item nav-link text-light header-text" href="/resume">Resume</a>
-      <a class="nav-item nav-link text-light header-text" href="/contact">Contact</a>
+    <ul class="navbar-nav">
+      <li class="nav-item nav-link text-light header-text" ><NavLink exact activeClassName="active" to='/about'>About</NavLink></li>
+      <li class="nav-item nav-link text-light header-text"><NavLink activeClassName="active" to='/portfolio'>Portfolio</NavLink></li>
+      <li class="nav-item nav-link text-light header-text"><NavLink activeClassName="active" to='/resume'>Resume</NavLink></li>
+      <li class="nav-item nav-link text-light header-text"><NavLink activeClassName="active" to='/contact'>Contact</NavLink></li>
 
-    </div>
+    </ul>
     </div>
   </div>
 </nav>
