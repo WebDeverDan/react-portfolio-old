15c941a11b635073dfc5b45c8fd617ac79142090
Daniel Bradley <danielbvideo@gmail.com>
2021-10-26 13:42:37 -0400

    moved skills to this page - will be reworking this page to look better and more high tech

--- main/src/components/Resume.js
+++ main/src/components/Resume.js
@@ -5,10 +5,14 @@ import { Container } from 'react-bootstrap';
 const Resume = () => {
   return (
     // <div class="container bg-dark bg-opacity-50 portfolio-container mt-5 mb-5">
-    <Container fluid="md bg-dark bg-opacity-75 mt-5 mb-5 resume-container">
+    <Container fluid="lg bg-dark bg-opacity-75 mt-5 mb-5 resume-container">
       <h1 class="display-4 text-light text-center">Resume</h1>
       <iframe src={Resume2} width="100%" height="500px ">
     </iframe>
+    <h2 class="lead display-4 text-light mt-3">Front End Skills</h2>
+    <p class="lead text-light display-6">HTML5, CSS, JQuery, JavaScript, Bootstrap, Materialize, Bulma, React, Handlebars.</p>
+    <h2 class="lead display-4 text-light">Back End Skills</h2>
+    <p class="lead text-light display-6 mb-3">Node.js, Express.js, MySQL, Sequelize, MongoDB, GraphQL, authentication and varification.</p>
       
     </Container >
       // </div>
